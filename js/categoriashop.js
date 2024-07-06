07ca1436b439cf8dd63d28253b1c81b5ddbb3468
vargascelerin <vargascelerin@gmail.com>
2024-07-06 16:47:50 -0600

    Agregué botones wishlist

--- js/categoriashop.js
+++ js/categoriashop.js
@@ -1,4 +1,8 @@
 function handleCategoryChange(category) {
     console.log('Category changed to:', category);
-    // Aquí se agregaría la funcionalidad de cada categoría
+    // Aquí se agregaría la funcionalidad de cada categoría cuando tengamos el back
+}
+function addToWishlist(event) {
+    event.preventDefault();
+    alert("Producto agregado a la lista de deseos");
 }
